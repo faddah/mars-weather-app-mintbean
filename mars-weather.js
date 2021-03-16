369715540650a3c6d2f42b41643a025d7cf642d7
Faddah Wolf <faddah@ksanahealth.com>
2021-03-16 13:32:15 -0700

    Wrapped in init(); added console.log(), verify runs; comment NASA API.

--- mars-weather.js
+++ mars-weather.js
@@ -1,14 +1,17 @@
 const init = () => {
-var obj;
-fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
-  .then(res => res.json())
-  .then(obj => {
-       sol = obj.sol_keys.slice(0,7) 
-      // console.log(sol)
-       console.log(obj)
-       //temperature = obj[sol].AT.av;
-       //aconsole.log(temperature)
-  })
-  .catch(e => console.log(e.message));
+    console.log(`mars b awll like, wazzzup, marvin?`);
+    var obj;
+    fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
+        .then(res => res.json())
+        .then(obj => {
+            sol = obj.sol_keys.slice(0, 7)
+                // console.log(sol)
+            console.log(obj)
+                //temperature = obj[sol].AT.av;
+                //aconsole.log(temperature)
+        })
+        .catch(e => console.log(e.message));
 }
 window.addEventListener('DOMContentLoaded', init);
+
+// API for NASA:  https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0
