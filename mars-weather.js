66f948f6e1801a302cb9a9ab41417da4917c96a9
Ruth Brand <ruthbrandsoprano@gmail.com>
2021-03-26 14:23:42 -0600

    adds html table

--- mars-weather.js
+++ mars-weather.js
@@ -1,7 +1,7 @@
 const init = () => {
     console.log(`mars b awll like, wazzzup, marvin?`);
     var obj;
-    fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
+    fetch('https://api.nasa.gov/insight_weather/?api_key=kOXVp1PTgPA1EcbLORoZMKoAQXamLVGpNeMCW4tJ&feedtype=json&ver=1.0')
         .then(res => res.json())
         .then(obj => {
             sol = obj.sol_keys.slice(0, 7)
@@ -13,5 +13,7 @@ const init = () => {
         .catch(e => console.log(e.message));
 }
 window.addEventListener('DOMContentLoaded', init);
+
+
 
 // API for NASA:  https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0
