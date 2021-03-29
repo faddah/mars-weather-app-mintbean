38d3e60b16ee21e981c9588d0103b8bb130a41db
Faddah Wolf <faddah@ksanahealth.com>
2021-03-29 00:59:47 -0700

    Akira's `getMarsWeather()`: fetch async/await + funcs. to place data.

--- mars-weather.js
+++ mars-weather.js
@@ -27,6 +27,39 @@ const init = () => {
     const readingsCount = document.querySelector('#readings-count');
     // console.log(dateSelector, readingsCount, avg, min, max, solDate);
 
+    const getMarsWeather = async() => {
+        try {
+            console.log("getting weather...");
+            const res = await fetch(marsAPI);
+            const weather = await res.json();
+            console.log("here's the weather");
+            console.log({ weather });
+            // console.log(weather)
+            const weatherKeys = Object.keys(weather);
+            // console.log(weatherKeys);
+            const marsWeather = weather[weatherKeys[1]]
+            const { Last_UTC, PRE, Season } = marsWeather;
+            // console.log(PRE);
+            const { av, ct, mn, mx } = PRE;
+            // console.log(Last_UTC, Season);
+            // console.log(av, ct, mn, mx);
+            const currentDate = new Date(Last_UTC);
+            currentSeason.textContent = capitalize(Season);
+            dateSelector.innerHTML = formattedDate(currentDate);
+            solDate.textContent = weatherKeys[1];
+            min.textContent = barometerFormatted(mn);
+            max.textContent = barometerFormatted(PRE.mx);
+            avg.textContent = barometerFormatted(PRE.av);
+            readingsCount.textContent = PRE.ct;
+        } catch (e) {
+            console.log(`yo! error, bro... ${e}`);
+            // console.log(e)
+        }
+    }
+
+    getMarsWeather();
+
+}
 window.addEventListener('DOMContentLoaded', init);
 
 
